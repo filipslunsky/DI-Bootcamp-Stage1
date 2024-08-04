45119377d2e0a276abbeb09ca28b194995fdce34
filipslunsky <fslunsky@gmail.com>
2024-08-04 16:54:20 +0200

    ExerciesXP Ninja completed

--- BootCamp/Week1/Day1/ExerciseXP-Ninja/Exercise5.py
+++ BootCamp/Week1/Day1/ExerciseXP-Ninja/Exercise5.py
@@ -1,8 +1,10 @@
-# tested_sentence = input("What is the longest sentence withou an A? ")
-# count = tested_sentence.count("a")
-
-count = 0
+tested_sentence = input("What is the longest sentence withou an A? ")
+count = tested_sentence.count("a")
+characters = 0
 
 while count == 0:
     characters = len(tested_sentence)
-    tested_sentence = input("Cool, your sentence has {characters} and no A. Can you do better? ")
+    tested_sentence = input(f"Cool, your sentence has {characters} and no A. Can you do better? ")
+    count = tested_sentence.count("a")
+
+print("GAME OVER, letter A detected")
