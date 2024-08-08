2663c50be092e3808b10472e45c91c8f3826b6ec
filipslunsky <fslunsky@gmail.com>
2024-08-08 11:01:19 +0200

    regular commit at the end of instruction part on W1D5

--- Python/D5/1_lambda_and_more.py
+++ Python/D5/1_lambda_and_more.py
@@ -31,3 +31,22 @@ numbers_squared = list(map(lambda number: number ** 2, numbers))
 print(numbers_squared)
 
 powered = lambda n, power : n ** power
+print(powered(4,5))
+
+# filter
+
+def starts_with_A(s):
+    return s[0] == "A"
+
+starts_A = list(filter(starts_with_A, fruits))
+print(starts_A)
+
+# reduce (needs to be imported from functools)
+from functools import reduce
+
+def sum_numbers(first, second):
+    return first + second
+
+my_list = [1, 3, 5, 6, 19]
+reduced_list = reduce(sum_numbers, my_list)
+print(reduced_list)
