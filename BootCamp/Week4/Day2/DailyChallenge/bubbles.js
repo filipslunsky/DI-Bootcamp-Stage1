6b90763737a8b21973cd7b8a60194346c867fb77
filipslunsky <fslunsky@gmail.com>
2024-08-26 16:01:13 +0200

    Gold Daily Challenge completed for W4D2

--- BootCamp/Week4/Day2/DailyChallenge/bubbles.js
+++ BootCamp/Week4/Day2/DailyChallenge/bubbles.js
@@ -1,8 +1,12 @@
 const numbers = [5,0,9,1,7,4,2,6,3,8];
-console.log(numbers)
 
+// 1. Using the .toString() method convert the array to a string.
 stringNumbers = numbers.toString()
 console.log(stringNumbers)
+
+
+// 2. Using the .join()method convert the array to a string. Try passing different values into the join.
+// Eg .join(“+”), .join(” “), .join(“”)
 joinNumbers = numbers.join("")
 console.log(joinNumbers)
 joinNumbers = numbers.join("+")
@@ -10,12 +14,22 @@ console.log(joinNumbers)
 joinNumbers = numbers.join("---")
 console.log(joinNumbers)
 
-let highestNumber = 0;
-for (let number of numbers) {
-    if (number > highestNumber) {
-        highestNumber = number
+// 3. Bonus : To do this Bonus look up how to work with nested for loops
+// Sort the numbers array in descending order, do so using for loops (Not using built-in sort methods).
+// The output should be: [9,8,7,6,5,4,3,2,1,0]
+
+let sortedNumbers = []
+while (numbers.length > 0) {
+        let highestNumber = 0;
+    for (let number of numbers) {
+        if (number > highestNumber) {
+            highestNumber = number
+        }
     }
-    
+    let highestNumberIndex = numbers.indexOf(highestNumber)
+    numbers.splice(highestNumberIndex, 1)
+    sortedNumbers.push(highestNumber)
+    highestNumber = 0
 }
 
-console.log(highestNumber)
+console.log(sortedNumbers)
