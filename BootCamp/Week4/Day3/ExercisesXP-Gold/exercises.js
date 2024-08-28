7ec6a2ab63f8b8891d14b63f3efbace898d2d9c5
filipslunsky <fslunsky@gmail.com>
2024-08-28 09:39:38 +0200

    regular commit during instruction day W4D4

---
+++ BootCamp/Week4/Day3/ExercisesXP-Gold/exercises.js
@@ -0,0 +1,43 @@
+// Exercise 1
+function isBlank(string) {
+    return (string === "")
+    }
+
+console.log(isBlank(""))
+console.log(isBlank("abc"))
+
+// Exercise 2
+function abbrevName (fullName) {
+    let nameList = fullName.split(" ")
+    let abbrevList = [nameList[0]];
+    for (let i = 1; i < nameList.length; i++) {
+        let lastName = nameList[i];
+        let lastNameAbbrev = lastName[0] + "."
+        abbrevList.push(lastNameAbbrev)
+    }
+    let abbrevNameString = abbrevList.join(" ");
+    return abbrevNameString;
+}
+
+console.log(abbrevName("Robin Singh"))
+
+// Exercise 3
+function swapCase(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let letter = sentence[i];
+        if (letter === letter.toUpperCase()) {
+            letter = letter.toLowerCase();
+            newSentence += letter;
+        } else {
+            letter = letter.toUpperCase();
+            newSentence += letter;
+        }
+    }
+    return newSentence;
+}
+
+
+console.log(swapCase("The Quick Brown Fox"))
+
+// Exercise 4
---
+++ JavaScript/03_DOM_events.js
@@ -0,0 +1,13 @@
+document.getElementById('button').onclick = function() {
+    alert('Click!');}
+
+// onclick()	
+// onload()	
+// onmouseover()
+// onmouseout()
+// onunload()
+
+// eventListener
+element.addEventListener(); // takes 2 params - event and a function triggered on that event (callback function), e.g. ("click", respondClick)
+element.removeEventListener();
+
