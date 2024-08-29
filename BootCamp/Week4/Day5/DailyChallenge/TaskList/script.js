82a3dd23f3ed5553c3b0c13ea5d0fec2fdde6d8b
filipslunsky <fslunsky@gmail.com>
2024-08-29 16:42:02 +0200

    first bonus part of TaskList completed

--- BootCamp/Week4/Day5/DailyChallenge/TaskList/script.js
+++ BootCamp/Week4/Day5/DailyChallenge/TaskList/script.js
@@ -1,4 +1,5 @@
 const tasks = [];
+let taskId = 0;
 
 let addButton = document.getElementById("add-button");
 
@@ -9,37 +10,64 @@ function addTask(e) {
     e.preventDefault();
     let inputValue = userInput.value.trim();
     if (inputValue !== "") {
-    tasks.push(inputValue)
-    
+    let task = {};
+    task.id = taskId;
+    taskId++;
+    task.text = inputValue;
+    task.done = false;
+    tasks.push(task);
+
     let newTaskDiv = document.createElement("div");
+    newTaskDiv.id = task.id;
     newTaskDiv.classList.add("task");
-    
     let taskList = document.getElementsByClassName("listTasks")[0];
     taskList.appendChild(newTaskDiv);
     
     let newXButton = document.createElement("button");
     newXButton.classList.add("button-x","red-x-button");
+    newXButton.id = task.id;
     newTaskDiv.appendChild(newXButton);
     
     let newIcon = document.createElement("i");
     newIcon.classList.add("fas", "fa-times");
+    newIcon.id = task.id;
     newXButton.appendChild(newIcon);
     
     let newCheckbox = document.createElement("input");
     newCheckbox.type = "checkbox";
     newCheckbox.name = "task-done";
     newCheckbox.classList.add("task-check");
+    newCheckbox.id = task.id;
+    newCheckbox.addEventListener("change", doneTask)
     newTaskDiv.appendChild(newCheckbox);
 
     newTaskText = document.createElement("p");
     newTaskText.textContent = inputValue;
+    newTaskText.id = task.id;
     newTaskDiv.appendChild(newTaskText);
     
     let newLine = document.createElement("hr");
     newLine.classList.add("blue-line");
+    newLine.id = task.id;
     newTaskDiv.appendChild(newLine);
     
     document.querySelector("form").reset();
 }
-
 }
+
+function doneTask(e) {
+    let taskId = e.target.id;
+    let fullTask = document.getElementById(taskId);
+    let taskText = fullTask.children[2];
+    for (let task of tasks) {
+        if (task.id == taskId) {
+            task.done = !task.done;
+            if (task.done) {
+                taskText.classList.add("crossed-out")
+            } else {
+                taskText.classList.remove("crossed-out")
+            }
+        }
+    }
+}
+
