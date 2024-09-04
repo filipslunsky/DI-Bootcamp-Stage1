f316e8769346d384887cb80e45805bf5c3444cd0
filipslunsky <fslunsky@gmail.com>
2024-09-04 11:47:30 +0200

    mandatory ExercisesXP completed for W5D4

---
+++ BootCamp/Week5/Day4/ExerciseXP/script.js
@@ -0,0 +1,34 @@
+// Exercise 3 : JSON Mario
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+console.log(marioGame);
+
+let marioGameJson = JSON.stringify(marioGame, undefined, 4); // pretty print
+
+console.log(marioGameJson);
+
+
+
+
