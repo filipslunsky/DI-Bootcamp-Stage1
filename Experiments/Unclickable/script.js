26db8cddbee7b18b465c73e89e4f5aaa39f82d3c
filipslunsky <fslunsky@gmail.com>
2024-09-05 01:46:00 +0200

    internal miniproject for my wife finished

--- Experiments/Unclickable/script.js
+++ Experiments/Unclickable/script.js
@@ -9,12 +9,21 @@ yesButton.addEventListener("click", function () {
     
 });
 
+let buttonWidth = 150;
+let buttonHeight = 50;
+let buttonTextSize = 15;
 noButton.addEventListener("mouseover", function () {
     noButton.style.position = "absolute";
     const randomInt1 = Math.floor(Math.random() * 500);
     const randomInt2 = Math.floor(Math.random() * 1100);
     noButton.style.top = `${randomInt1 + 30}px`;
     noButton.style.left = `${randomInt2 + 60}px`;
+    buttonWidth++;
+    buttonHeight++;
+    buttonTextSize++;
+    noButton.style.width = `${buttonWidth}px`;
+    noButton.style.height = `${buttonHeight}px`;
+    noButton.style.fontSize = `${buttonTextSize}px`;
 });
 
 noButton.addEventListener("click", function () {
