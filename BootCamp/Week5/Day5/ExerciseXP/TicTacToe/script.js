ccae51b372fad9d3189fd20a2c0f60757cac68e3
filipslunsky <fslunsky@gmail.com>
2024-09-05 21:40:53 +0200

    tic tac toe game logic complete

--- BootCamp/Week5/Day5/ExerciseXP/TicTacToe/script.js
+++ BootCamp/Week5/Day5/ExerciseXP/TicTacToe/script.js
@@ -26,85 +26,17 @@ const startGame = () => {
         document.getElementById(`field${i}`).addEventListener("click", (e) => {
             e.target.textContent = playerSide;
             e.target.style.pointerEvents = 'none';
-             if (evaluateStatus()) {
+            if (evaluateStatus()) {
                 computerPlay();
                 evaluateStatus();
-             };
+             }
         });
     }
 };
 
 const computerPlay = () => {
     let status = scanField();
-    if (((playerSide === status.f2 && playerSide === status.f3)
-    || (playerSide === status.f4 && playerSide === status.f7)
-    || (playerSide === status.f5 && playerSide === status.f9)) && ((status.f1 !== 'X') && (status.f1 !== 'O'))) {
-    console.log(1)
-    let field1 = document.getElementById("field1");
-    field1.textContent = computerSide;
-    field1.style.pointerEvents = 'none';
-    }
-    else if (((playerSide === status.f5 && playerSide === status.f8)
-        || (playerSide === status.f1 && playerSide === status.f3)) && ((status.f2 !== 'X') && (status.f2 !== 'O'))) {
-        console.log(2)
-    let field2 = document.getElementById("field2");
-    field2.textContent = computerSide;
-    field2.style.pointerEvents = 'none';
-    }
-    else if (((playerSide === status.f1 && playerSide === status.f2)
-        || (playerSide === status.f6 && playerSide === status.f9)
-        || (playerSide === status.f5 && playerSide === status.f7)) && ((status.f3 !== 'X') && (status.f3 !== 'O'))) {
-        console.log(3)
-    let field3 = document.getElementById("field3");
-    field3.textContent = computerSide;
-    field3.style.pointerEvents = 'none';
-    }
-    else if (((playerSide === status.f5 && playerSide === status.f6)
-        || (playerSide === status.f1 && playerSide === status.f7)) && ((status.f4 !== 'X') && (status.f4 !== 'O'))) {
-        console.log(4)
-    let field4 = document.getElementById("field4");
-    field4.textContent = computerSide;
-    field4.style.pointerEvents = 'none';
-    }
-    else if (((playerSide === status.f4 && playerSide === status.f6)
-        || (playerSide === status.f2 && playerSide === status.f8)
-        || (playerSide === status.f1 && playerSide === status.f9)
-        || (playerSide === status.f3 && playerSide === status.f7)) && ((status.f5 !== 'X') && (status.f5 !== 'O'))) {
-        console.log(5)
-    let field5 = document.getElementById("field5");
-    field5.textContent = computerSide;
-    field5.style.pointerEvents = 'none';
-    }
-    else if (((playerSide === status.f4 && playerSide === status.f5)
-        || (playerSide === status.f3 && playerSide === status.f9)) && ((status.f6 !== 'X') && (status.f6 !== 'O'))) {
-        console.log(6)
-    let field6 = document.getElementById("field6");
-    field6.textContent = computerSide;
-    field6.style.pointerEvents = 'none';
-    }
-    else if (((playerSide === status.f1 && playerSide === status.f4)
-        || (playerSide === status.f8 && playerSide === status.f9)
-        || (playerSide === status.f3 && playerSide === status.f5)) && ((status.f7 !== 'X') && (status.f7 !== 'O'))) {
-        console.log(7)
-    let field7 = document.getElementById("field7");
-    field7.textContent = computerSide;
-    field7.style.pointerEvents = 'none';
-    }
-    else if (((playerSide === status.f7 && playerSide === status.f9)
-        || (playerSide === status.f2 && playerSide === status.f5)) && ((status.f8 !== 'X') && (status.f8 !== 'O'))) {
-        console.log(8)
-    let field8 = document.getElementById("field8");
-    field8.textContent = computerSide;
-    field8.style.pointerEvents = 'none';
-    }
-    else if (((playerSide === status.f3 && playerSide === status.f6)
-        || (playerSide === status.f7 && playerSide === status.f8)
-        || (playerSide === status.f1 && playerSide === status.f5)) && ((status.f9 !== 'X') && (status.f9 !== 'O'))) {
-        console.log(9)
-    let field9 = document.getElementById("field9");
-    field9.textContent = computerSide;
-    field9.style.pointerEvents = 'none';
-    } else if (((computerSide === status.f2 && computerSide === status.f3)
+    if (((computerSide === status.f2 && computerSide === status.f3)
     || (computerSide === status.f4 && computerSide === status.f7)
     || (computerSide === status.f5 && computerSide === status.f9)) && ((status.f1 !== 'X') && (status.f1 !== 'O'))) {
     console.log(1)
@@ -172,6 +104,74 @@ const computerPlay = () => {
     let field9 = document.getElementById("field9");
     field9.textContent = computerSide;
     field9.style.pointerEvents = 'none';
+    } else if(((playerSide === status.f2 && playerSide === status.f3)
+    || (playerSide === status.f4 && playerSide === status.f7)
+    || (playerSide === status.f5 && playerSide === status.f9)) && ((status.f1 !== 'X') && (status.f1 !== 'O'))) {
+    console.log(1)
+    let field1 = document.getElementById("field1");
+    field1.textContent = computerSide;
+    field1.style.pointerEvents = 'none';
+    }
+    else if (((playerSide === status.f5 && playerSide === status.f8)
+        || (playerSide === status.f1 && playerSide === status.f3)) && ((status.f2 !== 'X') && (status.f2 !== 'O'))) {
+        console.log(2)
+    let field2 = document.getElementById("field2");
+    field2.textContent = computerSide;
+    field2.style.pointerEvents = 'none';
+    }
+    else if (((playerSide === status.f1 && playerSide === status.f2)
+        || (playerSide === status.f6 && playerSide === status.f9)
+        || (playerSide === status.f5 && playerSide === status.f7)) && ((status.f3 !== 'X') && (status.f3 !== 'O'))) {
+        console.log(3)
+    let field3 = document.getElementById("field3");
+    field3.textContent = computerSide;
+    field3.style.pointerEvents = 'none';
+    }
+    else if (((playerSide === status.f5 && playerSide === status.f6)
+        || (playerSide === status.f1 && playerSide === status.f7)) && ((status.f4 !== 'X') && (status.f4 !== 'O'))) {
+        console.log(4)
+    let field4 = document.getElementById("field4");
+    field4.textContent = computerSide;
+    field4.style.pointerEvents = 'none';
+    }
+    else if (((playerSide === status.f4 && playerSide === status.f6)
+        || (playerSide === status.f2 && playerSide === status.f8)
+        || (playerSide === status.f1 && playerSide === status.f9)
+        || (playerSide === status.f3 && playerSide === status.f7)) && ((status.f5 !== 'X') && (status.f5 !== 'O'))) {
+        console.log(5)
+    let field5 = document.getElementById("field5");
+    field5.textContent = computerSide;
+    field5.style.pointerEvents = 'none';
+    }
+    else if (((playerSide === status.f4 && playerSide === status.f5)
+        || (playerSide === status.f3 && playerSide === status.f9)) && ((status.f6 !== 'X') && (status.f6 !== 'O'))) {
+        console.log(6)
+    let field6 = document.getElementById("field6");
+    field6.textContent = computerSide;
+    field6.style.pointerEvents = 'none';
+    }
+    else if (((playerSide === status.f1 && playerSide === status.f4)
+        || (playerSide === status.f8 && playerSide === status.f9)
+        || (playerSide === status.f3 && playerSide === status.f5)) && ((status.f7 !== 'X') && (status.f7 !== 'O'))) {
+        console.log(7)
+    let field7 = document.getElementById("field7");
+    field7.textContent = computerSide;
+    field7.style.pointerEvents = 'none';
+    }
+    else if (((playerSide === status.f7 && playerSide === status.f9)
+        || (playerSide === status.f2 && playerSide === status.f5)) && ((status.f8 !== 'X') && (status.f8 !== 'O'))) {
+        console.log(8)
+    let field8 = document.getElementById("field8");
+    field8.textContent = computerSide;
+    field8.style.pointerEvents = 'none';
+    }
+    else if (((playerSide === status.f3 && playerSide === status.f6)
+        || (playerSide === status.f7 && playerSide === status.f8)
+        || (playerSide === status.f1 && playerSide === status.f5)) && ((status.f9 !== 'X') && (status.f9 !== 'O'))) {
+        console.log(9)
+    let field9 = document.getElementById("field9");
+    field9.textContent = computerSide;
+    field9.style.pointerEvents = 'none';
     } else {
     randomInt = Math.floor(Math.random() * 9) + 1;
     let randomKey = `f${randomInt}`;
@@ -196,6 +196,7 @@ const evaluateStatus = () => {
     || (playerSide === status.f1 && playerSide === status.f5 && playerSide === status.f9)
     || (playerSide === status.f3 && playerSide === status.f5 && playerSide === status.f7)) {
         console.log("You have won!");
+        finishGame();
         return false;
     } else if ((computerSide === status.f1 && computerSide === status.f2 && computerSide === status.f3)
         || (computerSide === status.f4 && computerSide === status.f5 && computerSide === status.f6)
@@ -206,6 +207,19 @@ const evaluateStatus = () => {
         || (computerSide === status.f1 && computerSide === status.f5 && computerSide === status.f9)
         || (computerSide === status.f3 && computerSide === status.f5 && computerSide === status.f7)) {
             console.log("You have lost!");
+            finishGame();
+        return false;
+    } else if ((status.f1 === 'X' || status.f1 === 'O')
+    && (status.f2 === 'X' || status.f2 === 'O')
+    && (status.f3 === 'X' || status.f3 === 'O')
+    && (status.f4 === 'X' || status.f4 === 'O')
+    && (status.f5 === 'X' || status.f5 === 'O')
+    && (status.f6 === 'X' || status.f6 === 'O')
+    && (status.f7 === 'X' || status.f7 === 'O')
+    && (status.f8 === 'X' || status.f8 === 'O')
+    && (status.f9 === 'X' || status.f9 === 'O')) {
+        console.log("The game is tied.");
+        finishGame();
         return false;
     } else {
         return true;
@@ -223,4 +237,10 @@ const scanField = () => {
         }
     }
     return fieldObj;
+};
+
+const finishGame = () => {
+    for (let i = 1; i <= 9; i++) {
+        document.getElementById(`field${i}`).style.pointerEvents = 'none';
+     }
 };
