fd5c3cae56b664916e962a7b071b472d794a4e82
filipslunsky <fslunsky@gmail.com>
2024-09-05 09:30:37 +0200

    miniproject Robots - JS card generation and filter completed

---
+++ BootCamp/Week5/Day5/ExerciseXP/RobotFriends/script.js
@@ -0,0 +1,94 @@
+const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      image: 'https://robohash.org/1?200x200'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      image: 'https://robohash.org/2?200x200'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      image: 'https://robohash.org/3?200x200'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      image: 'https://robohash.org/4?200x200'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+      image: 'https://robohash.org/5?200x200'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+      image: 'https://robohash.org/6?200x200'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+      image: 'https://robohash.org/7?200x200'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      image: 'https://robohash.org/8?200x200'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+      image:'https://robohash.org/9?200x200'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+      image:'https://robohash.org/10?200x200'
+    }
+];
+
+const displayCards = robotArray => {
+    let html = '';
+    robotArray.forEach(item => {
+        html += `<div class="card">
+            <img src = "${item.image}">
+            <p class = "robot-name">${item.name}</p>
+            <p class = "robot-email">${item.email}</p>
+        </div>`
+    });
+    document.getElementById('cards').innerHTML = html;
+};
+
+displayCards(robots);
+
+
+const searchInput = document.querySelector("input");
+searchInput.addEventListener("input", function () {
+    let searchValue = searchInput.value.trim().toLocaleLowerCase();
+    let filteredRobots = robots.filter(item => item.name.toLocaleLowerCase().includes(searchValue));
+    displayCards(filteredRobots);
+});
