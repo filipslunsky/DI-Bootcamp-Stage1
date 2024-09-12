ea99c43956b9c0aeeae1ef3d4c530dd80cc19725
filipslunsky <fslunsky@gmail.com>
2024-09-13 01:20:22 +0200

    middle of ExercisesXP for W6D5

--- BootCamp/Week6/Day5/ExerciseXP/blog-api/server.js
+++ BootCamp/Week6/Day5/ExerciseXP/blog-api/server.js
@@ -25,6 +25,8 @@ app.listen(PORT, () => {
     console.log(`listening to ${PORT}`)
 });
 
+app.use(express.json());
+
 // getting all blogs
 app.get('/posts', (req, res) => {
     res.json(data);
@@ -35,7 +37,27 @@ app.get('/posts/:id', (req, res) => {
     console.log(req.params);
     const { id } = req.params;
     const myBlog = data.find(item => item.id == id);
-    // finish handling wrong request
-    
+    if (!myBlog) return res.sendStatus(404);
     res.send(myBlog);
+});
+
+
+// adding a new blog
+app.post('/posts', (req, res) => {
+    const { title, content } = req.body;
+    const id = data.length + 1;
+    if (!title || !content) return res.sendStatus(400);
+    const newBlog = {id, title, content};
+    data.push(newBlog);
+    res.send('post completed');
+});
+
+// updating existing blog post
+app.put('/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    console.log(id);
+    console.log(title);
+    console.log(content);
+    res.send('success');
 });
