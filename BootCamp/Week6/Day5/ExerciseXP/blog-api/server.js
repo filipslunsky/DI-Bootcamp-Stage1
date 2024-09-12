5ae2466ca95040e2c5d6834684593493748ed831
filipslunsky <fslunsky@gmail.com>
2024-09-12 14:26:51 +0200

    middle of ExercisesXP for W6D5

---
+++ BootCamp/Week6/Day5/ExerciseXP/blog-api/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const data = [
+    {
+        id: 1,
+        title: 'Blog Title',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    },{
+        id: 2,
+        title: 'Another Blog Title',
+        content: 'Nihil, ea, voluptas nemo aut reiciendis ipsa quam esse excepturi libero dolore, numquam iusto illo quaerat vel? Nihil, ea, voluptas nemo aut reiciendis ipsa quam esse excepturi libero dolore, numquam iusto illo quaerat vel?'
+    },{
+        id: 3,
+        title: 'Yet Another Blog Title',
+        content: 'Dolore doloribus voluptate cupiditate commodi? Dolore doloribus voluptate cupiditate commodi? Dolore doloribus voluptate cupiditate commodi?'
+    }
+];
+
+// starting server to listen to port 3200
+const app = express();
+
+const PORT = 3200;
+
+app.listen(PORT, () => {
+    console.log(`listening to ${PORT}`)
+});
+
+// getting all blogs
+app.get('/posts', (req, res) => {
+    res.json(data);
+});
+
+// getting specific blog post
+app.get('/posts/:id', (req, res) => {
+    console.log(req.params);
+    const { id } = req.params;
+    const myBlog = data.find(item => item.id == id);
+    
+    
+    res.send(myBlog);
+});
