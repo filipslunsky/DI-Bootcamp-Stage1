c0068d11a1b16c27cbff6268960218244eace832
filipslunsky <fslunsky@gmail.com>
2024-09-26 10:41:50 +0200

    random quote generator for W8D5 completed

--- BootCamp/Week8/Day5/ExerciseXP/random-quotes-app/components/RandomQuote.jsx
+++ BootCamp/Week8/Day5/ExerciseXP/random-quotes-app/components/RandomQuote.jsx
@@ -1,5 +1,7 @@
 import quotes from "../src/assets/QuotesDatabase.jsx";
-import { useState, useEffect } from 'react';
+import colors from "../src/assets/ColorsDatabase.jsx";
+import { useState } from 'react';
+import './RandomQuote.css';
 
 const RandomQuote = (props) => {
     const getRandomQuote = () => {
@@ -9,21 +11,33 @@ const RandomQuote = (props) => {
         return randomQuote;
     };
 
+    const getRandomColour = () => {
+        const randInd = Math.floor(Math.random() * colors.length);
+        const randomColor = colors[randInd];
+        console.log(randomColor);
+        return randomColor;
+        
+    };
+
     const [quote, setQuote] = useState(getRandomQuote());
+    const [color, setColor] = useState(getRandomColour());
 
     const handleClick = () => {
         setQuote(getRandomQuote());
+        setColor(getRandomColour());
     };
-
-    useEffect(() => {
-        getRandomQuote();
-    }, []);
     
     return (
         <>
-            <h1>"{quote.quote}"</h1>
-            <p>- {quote.author.length === 0 ? 'unknown author' : quote.author} -</p>
-            <button onClick={handleClick}>New quote</button>
+            <div className="main" style={{width: 'max', height: '800px', backgroundColor: color}}>
+                <div className="container">
+                    <h1 style={{color: color}}>"{quote.quote}"</h1>
+                    <div className="align-right">
+                        <p style={{color: color}}>- {quote.author.length === 0 ? 'unknown author' : quote.author} -</p>
+                        <button style={{backgroundColor: color}} onClick={handleClick}>New quote</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
