68ad4a54dbd5ca6c294f6fcb1ef43713ffad1389
filipslunsky <fslunsky@gmail.com>
2024-10-13 11:00:31 +0200

    regular commit during instruction on W10D3

--- TypeScript/day3/build/main.js
+++ TypeScript/day3/build/main.js
@@ -1,5 +1,4 @@
 "use strict";
-// static method - addition from day2
 class UserS {
     static getCount() {
         return UserS.count;
@@ -18,3 +17,117 @@ console.log(user1.name, user1.id, UserS);
 console.log(user2.name, user2.id, UserS);
 console.log(user3.name, user3.id, UserS);
 console.log(UserS.getCount());
+;
+let employeeJohn = {
+    name: 'John',
+    age: 30,
+    role: 'developer',
+};
+console.log(employeeJohn.name);
+console.log(employeeJohn['age']);
+employeeJohn.email = 'jjj@gmail.com';
+employeeJohn.active = true;
+for (let key in employeeJohn) {
+    console.log(key, employeeJohn[key]);
+    console.log(key, employeeJohn[key]);
+    console.log(key, employeeJohn[key]);
+}
+;
+;
+let student1 = {
+    name: 'Jim',
+    grade: 'C',
+    courses: ['math', 'history'],
+};
+for (let key in student1) {
+    console.log(student1[key]);
+}
+;
+;
+let student2 = {
+    name: 'Jack',
+    grade: 2.2,
+    courses: ['english literature', 'coding'],
+};
+for (let key in student2) {
+    console.log(student2[key]);
+}
+;
+Object.keys(student1).forEach(key => {
+    console.log(key, student1[key]);
+    console.log(key, student1[key]);
+});
+Object.keys(student2).forEach(key => {
+    console.log(key, student2[key]);
+});
+;
+let employee1 = {
+    name: 'Anne',
+    age: 27,
+    role: 'User',
+};
+let employee2 = {
+    name: 'Amy',
+    age: 32,
+    role: 'User',
+};
+const strEcho = (value) => value;
+const numEcho = (value) => value;
+const boolEcho = (value) => value;
+const echo = (value) => value;
+const getFirstElementArray = (arr) => {
+    return arr[0];
+};
+console.log(getFirstElementArray([1, 2, 3, 4, 5, 6]));
+console.log(getFirstElementArray(['a', 'b', 'c']));
+console.log(getFirstElementArray([{ id: 1 }, { id: 2 }]));
+;
+const personOne = {
+    id: 1,
+    name: 'Marie',
+    age: 31,
+    info: ['female', 173, 'brown', true],
+};
+console.log(personOne);
+const personTwo = {
+    id: 2,
+    name: 'Ann',
+    age: 25,
+    info: {
+        city: 'tlv',
+        street: 'avivim',
+        num: 5,
+    },
+};
+const nums = [1, 2, 3];
+const strs = ['a', 'b', 'c'];
+const mergeArrays = (arr1, arr2) => {
+    return [...arr1, ...arr2];
+};
+console.log(mergeArrays(nums, strs));
+;
+const isObj = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (typeof arg === 'object' && !Array.isArray(arg) && arg !== null) {
+        return { arg, is: true };
+    }
+    return { arg, is: false };
+};
+console.log(isObj({ name: 'Dan', id: 2 }));
+console.log(isObj('asda'));
+console.log(isObj(123));
+console.log(isObj([1, 2, 3]));
+console.log(isObj(null));
+console.log(isObj([]));
+const post1 = {
+    id: 1,
+    title: 'my title',
+    body: 'bla bla bla',
+};
+const post2 = {
+    title: 'title 2',
+    body: 'whatever',
+};
+const updatePost = (post, update) => { };
--- TypeScript/day3/src/main.ts
+++ TypeScript/day3/src/main.ts
@@ -22,4 +22,206 @@ console.log(user2.name, user2.id, UserS);
 console.log(user3.name, user3.id, UserS);
 console.log(UserS.getCount());
 
+// ------------------- day3 ---------------------------
+// index signature / index key
+interface EmployeeI {
+    [key: string]: string | number | boolean // thanks to this, no need to write as in the loop
+    name: string;
+    age: number;
+    role: string;
+};
 
+let employeeJohn: EmployeeI = {
+    name: 'John',
+    age: 30,
+    role: 'developer',
+};
+
+console.log(employeeJohn.name);
+console.log(employeeJohn['age']);
+employeeJohn.email = 'jjj@gmail.com'; // - throws error on compile
+employeeJohn.active = true; // - throws error on compile
+// last two can only work if the dynmaic key is set in the interface
+
+for(let key in employeeJohn) {
+    console.log(key, employeeJohn[key as keyof EmployeeI]);  // tells TS what kind of key it is, without it - error on compile  
+    console.log(key, employeeJohn[key as keyof typeof employeeJohn]);  // tells TS what kind of key it is, without it - error on compile  
+    console.log(key, employeeJohn[key]);  
+};
+
+interface Student {
+    name: string;
+    grade: string | number;
+    courses?: string[];
+};
+
+let student1: Student = {
+    name: 'Jim',
+    grade: 'C',
+    courses: ['math', 'history'],
+};
+
+for (let key in student1) {
+    console.log(student1[key as keyof Student]);
+};
+
+interface StudentDynamic {
+    [key: string]: string| number | string[] | undefined
+    name: string;
+    grade: string | number;
+    courses?: string[];
+};
+
+let student2: StudentDynamic = {
+    name: 'Jack',
+    grade: 2.2,
+    courses: ['english literature', 'coding'],
+};
+
+for (let key in student2) {
+    console.log(student2[key]);
+};
+
+Object.keys(student1).forEach(key => {
+    console.log(key, student1[key as keyof typeof student1]);
+    console.log(key, student1[key as keyof Student]);
+});
+
+Object.keys(student2).forEach(key => {
+    console.log(key, student2[key]);
+});
+
+// record type
+interface Employee {
+    [key: string]: string | number | boolean // thanks to this, no need to write as in the loop
+    name: string;
+    age: number;
+    role: string;
+};
+
+let employee1: Employee = {
+    name: 'Anne',
+    age: 27,
+    role: 'User',
+};
+
+type keys = 'name' | 'age' | 'role';
+type EmployeeT = Record<keys, string | number>;
+
+let employee2: EmployeeT = {
+    name: 'Amy',
+    age: 32,
+    role: 'User',
+};
+
+// Generics
+const strEcho = (value: string): string => value;
+const numEcho = (value: number): number => value;
+const boolEcho = (value: boolean): boolean => value;
+
+const echo = <T>(value: T): T => value;
+
+const getFirstElementArray = <T>(arr: T[]): T => {
+    return arr[0];
+};
+
+console.log(getFirstElementArray([1, 2, 3, 4, 5, 6]));
+console.log(getFirstElementArray(['a', 'b', 'c']));
+console.log(getFirstElementArray([{id: 1}, {id: 2}]));
+
+interface Person<T> {
+    id: number;
+    name: string;
+    age: number;
+    info: T;
+};
+
+const personOne: Person<(number | string | boolean)[]> = {
+    id: 1,
+    name: 'Marie',
+    age: 31,
+    info: ['female', 173, 'brown', true],
+};
+
+console.log(personOne);
+
+type Info = {
+    city: string;
+    street: string;
+    num: number;
+};
+
+const personTwo: Person<Info> = {
+    id: 2,
+    name: 'Ann',
+    age: 25,
+    info: {
+        city: 'tlv',
+        street: 'avivim',
+        num: 5,
+    },
+};
+
+// more than 1 generic
+const nums: number[] = [1, 2, 3];
+const strs: string[] = ['a', 'b', 'c'];
+
+const mergeArrays = <T, K>(arr1: T[], arr2: K[]): (T | K)[] => {
+    return [...arr1, ...arr2];
+};
+
+console.log(mergeArrays(nums, strs));
+
+// const isObj = <T>(item: T): boolean => {
+//     return typeof item === 'object' && item !== null && !Array.isArray(item);
+// };
+
+// console.log(isObj({name: 'Dan', id: 2}));
+// console.log(isObj('asda'));
+// console.log(isObj(123));
+// console.log(isObj([1, 2, 3]));
+// console.log(isObj(null));
+
+interface BoolCheck<T> {
+    arg: T;
+    is: boolean;
+};
+
+const isObj = <T>(arg: T): BoolCheck<T> => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false }
+    }
+    if (typeof arg === 'object' && !Array.isArray(arg) && arg !== null) {
+        return { arg, is: true }
+    }
+    return { arg, is: false }
+};
+
+console.log(isObj({name: 'Dan', id: 2}));
+console.log(isObj('asda'));
+console.log(isObj(123));
+console.log(isObj([1, 2, 3]));
+console.log(isObj(null));
+console.log(isObj([]));
+
+// utility type
+type PostT = {
+    id: number;
+    title: string;
+    body: string;
+};
+
+type PartialPost = Partial<PostT>;
+
+const post1: PostT = {
+    id: 1,
+    title: 'my title',
+    body: 'bla bla bla',
+};
+
+const post2: PartialPost = {
+    title: 'title 2',
+    body: 'whatever',
+};
+
+const updatePost = (post: PostT, update: PartialPost) => {};
